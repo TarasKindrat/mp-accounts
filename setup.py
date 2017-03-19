f8499e5f36f8cf15b2f193531312ee85785ecfc4
Paul Maigutyak <pmaigutyak@ettera.com>
2017-03-19 16:04:55 +0200

    Update setup files.

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup, find_packages
+
+from accounts import __version__
 
 
 setup(
-    name='accounts',
-    version='1.0.0',
+    name='django-mp-accounts',
+    version=__version__,
     description='',
     long_description=open('README.md').read(),
     author='Paul Maigutyak',
     author_email='pmaigutyak@gmail.com',
     url='https://github.com/pmaigutyak/mp-accounts',
-    packages=['accounts'],
+    download_url='https://github.com/pmaigutyak/mp-accounts/archive/%s.tar.gz' % __version__,
+    packages=find_packages(),
     license='MIT',
     install_requires=[
         'django-allauth',
